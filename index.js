e11f835d409a7206b004ba2ed264916224cc6713
unknown <imalriyad@gmail.com>
2024-11-28 22:14:27 +0600

    initial server setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const app = express();
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db'); 
+
+
+// Middleware 
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+connectDB()
+
+
+app.get("/",(req,res)=>{
+     res.send({message: "passthefood server running..."});
+})
+
+app.listen(port,()=>{
+    console.log("passthefood server running on port", port);
+    
+})
+
+module.exports = app
